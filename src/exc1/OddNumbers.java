f7a95186f60f27775fb94b4f6800afb1ef5a1f1a
lilla <lilla@quince.hu>
2018-03-20 23:06:28 +0100

    defining odd and even

--- src/exc1/OddNumbers.java
+++ src/exc1/OddNumbers.java
@@ -19,6 +19,33 @@ public class OddNumbers {
         nbr5 = input.nextInt();
         input.close();
 
+        if ( nbr1 % 2 == 0 )
+            System.out.println(nbr1 + " number is even");
+        else
+            System.out.println(nbr1 + " number is odd");
+
+        if ( nbr2 % 2 == 0 )
+            System.out.println(nbr2 + " number is even");
+        else
+            System.out.println(nbr2 + " number is odd");
+
+        if ( nbr3 % 2 == 0 )
+            System.out.println(nbr3 + " number is even");
+        else
+            System.out.println(nbr3 + " number is odd");
+
+        if ( nbr4 % 2 == 0 )
+            System.out.println(nbr4 + " number is even");
+        else
+            System.out.println(nbr4 + " number is odd");
+
+        if ( nbr5 %  2 == 0 )
+            System.out.println(nbr5 + " number is even");
+        else
+            System.out.println(nbr5 + " number is odd");
+
+
+
         avarage = (double) (nbr1+nbr2+nbr3+nbr4+nbr5)/5;
 
         System.out.println("The avarage number is:" + avarage);
